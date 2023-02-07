f6f09bb68ff8acf1480b11bb360b943db9990529
judkins8 <joshua.judkins@gmail.com>
2023-02-07 00:05:34 -0700

    bugs

--- Mission5/wwwroot/js/JavaScript.js
+++ Mission5/wwwroot/js/JavaScript.js
@@ -1,35 +1,64 @@
 ﻿// click sumbit button triggers java script
 $("#calculateGrade").click(function () {
-    // calc the overall percentage
-    let percentage = (($("#assignments").val() * .5) + ($("#groupProject").val() * .1) + ($("#quizzes").val() * .1) +
-        ($("#midterm").val() * .1) + ($("#final").val() * .1) + ($("#intex").val() * .1));
+    //grab values from form
+    var A = parseFloat($("#A").val());
+    var GP = parseFloat($("#GP").val());
+    var Q = parseFloat($("#Q").val());
+    var MX = parseFloat($("#MX").val());
+    var FX = parseFloat($("#FX").val());
+    var Intex = parseFloat($("#Intex").val());
 
-    // calc letter grade
-    if (percentage >= 94) {
-        grade = "A";
-    } else if (percentage >= 90) {
-        grade = "A-"
-    } else if (percentage >= 87) {
-        grade = "B+"
-    } else if (percentage >= 84) {
-        grade = "B"
-    } else if (percentage >= 80) {
-        grade = "B-"
-    } else if (percentage >= 77) {
-        grade = "C+"
-    } else if (percentage >= 74) {
-        grade = "C"
-    } else if (percentage >= 70) {
-        grade = "C-"
-    } else if (percentage >= 67) {
-        grade = "D+"
-    } else if (percentage >= 64) {
-        grade = "D"
-    } else if (percentage >= 60) {
-        grade = "D-"
-    } else {
-        grade = "E"
-    };
-    //send to index.html to print
-    $("#Output").html("Your grade is " + grade + " with " + percentage + "%");
+    //calculations for grade
+    var Aper = A * .5
+    var GPper = GP * .1
+    var Qper = Q * .1
+    var MXper = MX * .1
+    var FXper = FX * .1
+    var Intexper = Intex * .1
+    var letterGrade = ''
+
+    var finalGrade = Aper + GPper + Qper + MXper + FXper + Intexper
+
+    finalGrade = finalGrade.toFixed(2)
+
+    //if statement to assign letter grade
+    if (finalGrade >= 94) {
+        letterGrade = 'A'
+    }
+    else if (finalGrade >= 90) {
+        letterGrade = 'A-'
+    }
+    else if (finalGrade >= 87) {
+        letterGrade = 'B+'
+    }
+    else if (finalGrade >= 84) {
+        letterGrade = 'B'
+    }
+    else if (finalGrade >= 80) {
+        letterGrade = 'B-'
+    }
+    else if (finalGrade >= 77) {
+        letterGrade = 'C+'
+    }
+    else if (finalGrade >= 74) {
+        letterGrade = 'C'
+    }
+    else if (finalGrade >= 70) {
+        letterGrade = 'C-'
+    }
+    else if (finalGrade >= 67) {
+        letterGrade = 'D+'
+    }
+    else if (finalGrade >= 64) {
+        letterGrade = 'D'
+    }
+    else if (finalGrade >= 60) {
+        letterGrade = 'D-'
+    }
+    else {
+        letterGrade = 'E'
+    }
+
+    //display results
+    $('#letter').text("Your final grade is " + letterGrade + " and your percentage is " + finalGrade + "%")
 })
